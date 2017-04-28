475c5caac471fc08d9e40bf273652e114ee875bb
Akai-Kumako <e1433@s.akashi.ac.jp>
2017-04-28 09:36:18 +0900

    文字の出現回数カウント

--- ngramTest.py
+++ ngramTest.py
@@ -1,21 +1,30 @@
-#!/user/bin/env python
 # coding: utf-8
 
 import os.path
 import codecs
 
+x = 1
+
 def ngram(text, n):
 	results = []
-	x = 0
+	global x
 	if len(text) >= n:
 		for i in range(len(text)-n+1):
 			results.append(text[i:i+n])
 			x+=1
-	print(x)
 	return results
 
 f = codecs.open('data/001.txt', 'r', 'utf-8')
 text = f.read()
 
-for e in ngram(text, 3):
-	print(e)
+words = {}
+for word in ngram(text, 3):
+	words[word] = words.get(word, 0) + 1
+
+d = [(v, k) for k , v in words.items()]
+d.sort()
+d.reverse()
+for count, word in d:
+	print count, word 
+
+print(x)
